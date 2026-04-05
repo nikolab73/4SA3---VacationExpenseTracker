28155c11a31de1cc587bf05c43acdef4570f2a1d
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-04-04 23:44:11 -0400

    added inputs for entry details, added file to control db collumns and types

--- VacationExpenseTracker.py
+++ VacationExpenseTracker.py
@@ -21,6 +21,7 @@ def connect_db():
 
 def main_menu():
     """Display main menu & get users choice on actions"""
+
     print("\nWelcome to the Vacation Expense Tracker\nPlease select which actions you would like to do" + "="*55)
     print("""1. Create new trip
           2. Work on existing trip
@@ -30,6 +31,18 @@ def main_menu():
 
 def new_trip():
     """Creating a new trip"""
+
+    print("\nBeginning a new trip is exciting!\nLet's start with some details about your trip")
+    trip_name = input("Please name your trip, make sure to remember this: ")
+    home_currency = input("Please enter your home currency (e.g., CAD): ").upper()
+    trip_currency = input("Please enter the currency used on your trip (e.g., EUR): ").upper()
+    budget = input("Please enter your trip budget (in home currency): ")
+
+    print("\nThank you! Now let's get started on your first expense")
+    amount = float(input(f"Enter expense amount (in ${trip_currency}): " ))
+    category = input("Enter category for spending (food, stay, transport, event): ")
+    
+
 
 
 def main():
