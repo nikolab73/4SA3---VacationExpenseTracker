ab4039d5299bff23741b6d8172ff0b6e4f57bf83
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-04-05 00:35:37 -0400

    pushing (1st) entry to db

--- VacationExpenseTracker.py
+++ VacationExpenseTracker.py
@@ -2,6 +2,7 @@
 
 import requests
 import psycopg2
+from datetime import date
 
 # API initialization
 url = "https://api.exchangerate-api.com/v4/latest/USD"
@@ -29,24 +30,39 @@ def main_menu():
     selection = input("Enter choice (1, 2, 3): ")
     return selection
 
-def new_trip():
+def new_trip(connection, cursor):
     """Creating a new trip"""
 
+    #adding trip details
     print("\nBeginning a new trip is exciting!\nLet's start with some details about your trip")
     trip_name = input("Please name your trip, make sure to remember this: ")
     home_currency = input("Please enter your home currency (e.g., CAD): ").upper()
     trip_currency = input("Please enter the currency used on your trip (e.g., EUR): ").upper()
     budget = input("Please enter your trip budget (in home currency): ")
 
+    # adding expense details
     print("\nThank you! Now let's get started on your first expense")
     amount = float(input(f"Enter expense amount (in ${trip_currency}): " ))
     category = input("Enter category for spending (food, stay, transport, event): ")
-    
 
+    # calculate converted amounts
+    if home_currency != trip_currency:
+        amount_converted = amount*1.10 # placeholder for conversion
+
+    # append 1st entry to db
+    cursor.execute("""INSERT INTO expenses
+                   (trip_name, home_currency, trip_currency, trip_budget, amount, amount_converted, category, expense_date)
+                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                   """, (trip_name, home_currency, trip_currency, budget, amount, amount_converted, category, date.today()))
 
 
 def main():
     """Main program loop"""
+
+    DBconnection = connect_db()
+    cursor = DBconnection.cursor
+
+    new_trip(DBconnection, cursor)
 
 if __name__ == "__main__":
     main()
