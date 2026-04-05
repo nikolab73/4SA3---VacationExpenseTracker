fdfadf92f0ad62ab25d5f2a5b5f31dbc5004868f
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-04-04 22:13:29 -0400

    properly replaced table

--- VacationExpenseTracker.py
+++ VacationExpenseTracker.py
@@ -20,14 +20,33 @@ connection = psycopg2.connect(
 cursor = connection.cursor()
 
 # Create table
+
+# Drop old table if it exists
+cursor.execute("DROP TABLE IF EXISTS expenses")
+
+# Create single expenses table with trip info
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS expenses(
-        id SERIAL PRIMARY KEY,
-        amount float,
-        category VARCHAR(50)
+    CREATE TABLE expenses(
+        expense_id SERIAL PRIMARY KEY,
+        category VARCHAR(50),
+        expense_date VARCHAR(50),
+               
+        amount DECIMAL(10,2),
+        amount_converted DECIMAL(10,2),
+               
+        home_currency VARCHAR(3),
+        trip_currency VARCHAR(3),
+               
+        trip_name VARCHAR(50),
+        trip_budget DECIMAL(10,2)
     )
 """)
 
 # main program
 
 print("Welcome to the Vacation Expense Tracker \nWould you like to create a new trip or ")
+
+cursor.execute("INSERT INTO expenses (amount, category) VALUES (50.01, 'food')")
+
+connection.commit()
+connection.close()
